badf2beca9d392d096435b38cedfc423a40db2c5
Emily Huang <144239497+EmilyHuang111@users.noreply.github.com>
2024-11-02 14:56:25 -0400

    Create main.py

---
+++ Maximum of Two Values/main.py
@@ -0,0 +1,10 @@
+def maximum(x,y):
+  if (x > y):
+    return x
+  else:
+    return y
+
+number_1 = int(input("Please input the first integer value: "))
+number_2 = int(input("Please input the second integer value: "))
+
+print(maximum(number_1, number_2))
